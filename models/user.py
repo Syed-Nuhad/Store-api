470055d61f2361bb370e923d9423e90a0842fa66
PC <nuhad7july02@gmail.com>
2025-07-19 13:51:04 +0600

    fixed a bug in stores🪰

--- models/user.py
+++ models/user.py
@@ -4,3 +4,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
